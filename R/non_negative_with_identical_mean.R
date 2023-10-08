47805dcbf2bfffad98eaaff0ec920cafc11a7331
Tim Radtke <tim@timradtke.net>
2023-10-08 13:37:13 +0200

    Add `non_negative_with_identical_mean()`

---
+++ R/non_negative_with_identical_mean.R
@@ -0,0 +1,49 @@
+#' Postprocess function making forecast samples non-negative while keeping the
+#' mean at the same value as it was before through scaling of the samples 
+#' (unless the sample mean was originally negative).
+#' 
+#' While the identical sample mean can be enforced, this will generally decrease
+#' the sample variance as this is the only way to maintain the mean while
+#' turning negative samples to zero to enforce the non-negativity constraint.
+#' 
+#' The returned samples will always be non-negative. The sample mean will not be
+#' identical if it cannot be achieved given the non-negative samples.
+#' 
+#' @param x A numeric matrix of dimensions (k, 1)
+#' 
+#' @return A numeric matrix of dimensions (k, 1)
+#' 
+#' @seealso [predict.threedx()]
+#' 
+#' @export
+#' @examples
+#' non_negative_with_identical_mean(x = matrix(c(-1, 0, 1, 2), ncol = 1))
+#' 
+#' x <- matrix(rnorm(100, 0.5), ncol = 1)
+#' summary(x)
+#' summary(non_negative_with_identical_mean(x))
+#' 
+#' x <- matrix(rnorm(100, -1), ncol = 1)
+#' summary(x)
+#' # can't keep identical mean as original mean is negative
+#' summary(non_negative_with_identical_mean(x))
+#' 
+non_negative_with_identical_mean <- function(x) {
+  checkmate::assert_matrix(x = x, min.cols = 1, max.cols = 1)
+  if (nrow(x) == 0) {
+    return(x)
+  }
+  if (anyNA(x)) {
+    x[, 1] <- NA_real_
+    return(x)
+  }
+  
+  x_non_negative <- x
+  x_non_negative[, 1] <- pmax(0, x)
+  
+  if (mean(x_non_negative) == 0 || mean(x) < 0) {
+    return(x_non_negative)
+  }
+  
+  x_non_negative / mean(x_non_negative) * mean(x)
+}
---
+++ tests/testthat/test-non_negative_with_identical_mean.R
@@ -0,0 +1,51 @@
+library(checkmate)
+
+test_that("returns non-negative version of input with identical sample mean if sample mean is positive", {
+  x <- matrix(data = rnorm(n = 100, mean = 0.5), ncol = 1)
+  x_non_negative <- non_negative_with_identical_mean(x = x)
+  
+  expect_equal(mean(x), mean(x_non_negative))
+  expect_numeric(x = as.numeric(x_non_negative), lower = 0)
+  expect_matrix(x = x_non_negative, nrows = nrow(x), ncols = 1)
+})
+
+test_that("returns input if input is non-negative", {
+  x <- matrix(data = rpois(n = 10, lambda = 1), ncol = 1)
+  x_non_negative <- non_negative_with_identical_mean(x = x)
+  expect_identical(x_non_negative, x)
+})
+
+test_that("returns non-negative with different mean if input mean is negative", {
+  x <- matrix(data = c(-2, -1, 0, -0.5, -5), ncol = 1)
+  x_non_negative <- non_negative_with_identical_mean(x = x)
+  x[,1] <- 0
+  expect_identical(x_non_negative, x)
+})
+
+test_that("returns all-zero if input mean is zero", {
+  x <- matrix(data = c(-2, -1, 0, 1, 2), ncol = 1)
+  x_non_negative <- non_negative_with_identical_mean(x = x)
+  x[,1] <- 0
+  expect_identical(x_non_negative, x)
+})
+
+test_that("returns all-NA if input mean is NA", {
+  x <- matrix(data = c(-2, NA_real_, 0, 1, 2), ncol = 1)
+  x_non_negative <- non_negative_with_identical_mean(x = x)
+  x[,1] <- NA_real_
+  expect_identical(x_non_negative, x)
+})
+
+test_that("is idempotent", {
+  x <- matrix(data = rnorm(n = 100, mean = 0.5), ncol = 1)
+  x_one <- non_negative_with_identical_mean(x = x)
+  x_two <- non_negative_with_identical_mean(x = x_one)
+  
+  expect_identical(x_two, x_one)
+})
+
+test_that("returns input if input has zero rows", {
+  x <- matrix(ncol = 1, nrow = 0)
+  x_non_negative <- non_negative_with_identical_mean(x = x)
+  expect_identical(x_non_negative, x)
+})
